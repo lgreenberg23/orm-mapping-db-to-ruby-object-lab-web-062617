8ae3d74a8f681db49786374683c971bb96ec0983
github username <github email address>
2017-07-05 22:58:01 -0400

    Done.

--- lib/student.rb
+++ lib/student.rb
@@ -1,18 +1,49 @@
 class Student
   attr_accessor :id, :name, :grade
 
+
+  # def initialize(name, grade, id = nil)
+  #   @name = name
+  #   @grade = grade
+  #   @id = id
+  # end
+
   def self.new_from_db(row)
+
+    new_student = self.new
+    new_student.id = row[0]
+    new_student.name = row[1]
+    new_student.grade = row[2]
+    new_student    
     # create a new Student object given a row from the database
   end
 
   def self.all
     # retrieve all the rows from the "Students" database
     # remember each row should be a new instance of the Student class
+    sql = <<-SQL
+      SELECT *
+      FROM students
+    SQL
+ 
+    DB[:conn].execute(sql).map do |row|
+        self.new_from_db(row)
+    end
   end
+
 
   def self.find_by_name(name)
     # find the student in the database given a name
     # return a new instance of the Student class
+    sql = <<-SQL
+      SELECT *
+      FROM students WHERE name = ?
+      LIMIT 1
+    SQL
+
+    DB[:conn].execute(sql, name).map do |row|
+      self.new_from_db(row)
+    end.first
   end
   
   def save
@@ -40,4 +71,46 @@ class Student
     sql = "DROP TABLE IF EXISTS students"
     DB[:conn].execute(sql)
   end
+
+  def self.count_all_students_in_grade_9
+    sql = <<-SQL
+      SELECT COUNT(name) FROM students 
+      WHERE grade = 9
+    SQL
+      DB[:conn].execute(sql)
+  end
+
+  def self.students_below_12th_grade
+    sql = <<-SQL
+      SELECT COUNT(name) FROM students WHERE grade < 12
+    SQL
+    DB[:conn].execute(sql)
+  end
+
+  def self.first_x_students_in_grade_10(x)
+    sql = <<-SQL
+      SELECT * FROM students WHERE grade = 10 LIMIT #{x};
+    SQL
+    DB[:conn].execute(sql)
+  end
+
+  def self.first_student_in_grade_10
+    sql = <<-SQL
+      SELECT * FROM students WHERE grade = 10 ORDER BY id LIMIT 1;
+    SQL
+    row = DB[:conn].execute(sql).first
+    self.new_from_db(row)
+  end
+
+  def self.all_students_in_grade_x(x)
+    sql = <<-SQL
+      SELECT * FROM students WHERE grade = #{x}
+    SQL
+    DB[:conn].execute(sql)
+  end
+
+
 end
+
+
+# @id = DB[:conn].execute("SELECT last_insert_rowid() FROM students")[0][0]
